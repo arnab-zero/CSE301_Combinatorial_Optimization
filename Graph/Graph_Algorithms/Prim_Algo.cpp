99b9dfb7baca33f6cde2e59ff09a32a575ad6daa
Arnab Das Joy <darnabhere@gmail.com>
2023-04-07 01:24:10 +0600

    Added source code and sample i/o for Prim's algo.

--- Graph/Graph_Algorithms/Prim_Algo.cpp
+++ Graph/Graph_Algorithms/Prim_Algo.cpp
@@ -9,18 +9,41 @@ using namespace std;
 typedef long long int lli;
 
 priority_queue<pii, vector<pii>, greater<pii>> pq;
-vector<bool> vis(100, false);       // marks if a vertex is already visited or not
+vector<bool> vis(20, false);       // marks if a vertex is already visited or not
+vector<int> weight(20, INT_MAX);
+vector<int> parent(20, -1);
 
 void mst_prim(vector< vector<pair<int, int> > > &adj_list){
 
+    while(!pq.empty()){
+        pair<int, int> current = pq.top();
+        pq.pop();
+
+        if(vis[current.second])
+            continue;
+        
+        vis[current.second] = true;
+
+        for(auto i: adj_list[current.second]){
+            if(weight[i.second] > i.first and !vis[i.second]){
+                weight
+[i.second] = i.first;
+                parent[i.second] = current.second;
+                pq.push({i.first, i.second}); 
+            }
+        }
+    }
 }
 
 int main(void){
 
+    freopen("Prim_Algo_input.txt", "r", stdin);
+    freopen("Prim_Algo_output.txt", "w", stdout);
+
     int vertices, edges;
     cin>>vertices>>edges;
 
-    vector< vector< pair<int, int> > > adj_list(vertices, vector<pair<int, int>>());
+    vector< vector< pair<int, int> > > adj_list(vertices+1);
 
     // graph input as adjacency list
     for(int i=0; i<edges; ++i){
@@ -31,12 +54,29 @@ int main(void){
         adj_list[v].push_back({w,u});
     }
 
-    int start;
-    cin>>start;
+    cout<<"Considering vertex 1 as the root vertex."<<endl<<endl;
+    pq.push({0, 1});
 
-    pq.push({0, start});
+    // cout<<pq.top().second<<endl;
+    // for(auto i: adj_list[pq.top().second]){
+    //     cout<<i.second<<spc<<i.first<<endl;
+    // }
+    // for(int i=1; i<vertices+1; ++i){
+    //     cout<<"vertex: "<<i<<endl;
+    //     for(auto p:adj_list[i]){
+    //         cout<<p.second<<spc<<p.first<<endl;
+    //     }
+    //     cout<<endl;
+    // }
 
-    mst_prim(adj_list);             // Function call for prim's algo 
+     mst_prim(adj_list);             // Function call for prim's algo 
+
+    for(int i=1; i<vertices+1; ++i){
+        cout<<"vertex "<<(char)(i+'a'-1)<<": ";
+        cout<<" vis: "<<vis[i]<<tab;
+        cout<<" weight: "<<weight[i]<<tab;
+        cout<<" parent: "<<(char)(parent[i]+'a'-1)<<endl;
+    }
 
     return 0;       
 }
