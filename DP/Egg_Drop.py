c04ef168f0685f2ef5180417c2ecd53513d09671
Arnab Das Joy <darnabhere@gmail.com>
2023-04-11 08:09:06 +0600

    Added source code.

---
+++ DP/Egg_Drop.py
@@ -0,0 +1,24 @@
+
+def eggDrop(n, k):  
+    dp = [[0 for i in range(k + 1)] for j in range(n + 1)] 
+
+    for i in range(1, n + 1): 
+        dp[i][1] = 1
+        dp[i][0] = 0
+
+    for j in range(1, k + 1): 
+        dp[1][j] = j 
+
+    for i in range(2, n + 1): 
+        for j in range(2, k + 1): 
+            dp[i][j] = float('inf')
+            for x in range(1, j + 1): 
+                res = 1 + max(dp[i-1][x-1], dp[i][j-x]) 
+                if res < dp[i][j]: 
+                    dp[i][j] = res 
+
+    return dpeggFloor[n][k] 
+
+n = 2
+k = 36
+print(eggDrop(n, k))
