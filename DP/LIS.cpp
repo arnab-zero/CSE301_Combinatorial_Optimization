60818d2bc4ca5656af8c8d5107083fead555789f
Arnab Das Joy <darnabhere@gmail.com>
2023-04-11 08:59:18 +0600

    Added source code for LIS.

--- DP/LIS.cpp
+++ DP/LIS.cpp
@@ -1,70 +1,56 @@
-/*
-Code: Longest increasing subsequence using dp;
-Time complexity: O(n*n);
-01:21, jan 2nd, 2023;
-*/
+#include <iostream>
+#include <vector>
 
-#include <bits/stdc++.h>
-using namespace std;
+// Binary search (note boundaries in the caller)
+int CeilIndex(std::vector<int>& v, int l, int r, int key)
+{
+	while (r - l > 1) {
+		int m = l + (r - l) / 2;
+		if (v[m] >= key)
+			r = m;
+		else
+			l = m;
+	}
 
-#define spc " "
-#define tab "\t"
+	return r;
+}
 
+int LongestIncreasingSubsequenceLength(std::vector<int>& v)
+{
+	if (v.size() == 0)
+		return 0;
 
-int main(void){
-    int n;
-    cout<<"Enter number of inputs: ";
-    cin>>n;
+	std::vector<int> tail(v.size(), 0);
+	int length = 1; // always points empty slot in tail
 
-    int input[n], prev[n], length[n];
-    int maxLengthIndex = 0, maxLength = 0;
+	tail[0] = v[0];
+	for (size_t i = 1; i < v.size(); i++) {
 
-    for(int i=0; i<n; ++i){
-        cin>>input[i];
-        prev[i] = -1;
-        length[i] = 1; 
-    }
+		// new smallest value
+		if (v[i] < tail[0])
+			tail[0] = v[i];
 
-    // for(int i=0; i<n; ++i){
-    //     cout<<i+1<<tab<<input[i]<<tab<<prev[i]<<tab<<length[i]<<endl;
-    // }
+		// v[i] extends largest subsequence
+		else if (v[i] > tail[length - 1])
+			tail[length++] = v[i];
 
-    for(int i=0; i<n; ++i){
-        int tempLength = 0, tempIndex = -1;
+		// v[i] will become end candidate of an existing
+		// subsequence or Throw away larger elements in all
+		// LIS, to make room for upcoming greater elements
+		// than v[i] (and also, v[i] would have already
+		// appeared in one of LIS, identify the location
+		// and replace it)
+		else
+			tail[CeilIndex(tail, -1, length - 1, v[i])] = v[i];
+	}
 
-        for(int j=0; j<i; ++j)
-        {
-            if(tempLength < length[j] && input[j] < input[i])
-                tempIndex = j;
-            else if(tempLength == length[j] && input[j] < input[i] && input[j]<input[tempIndex])
-                tempIndex = j; 
-        }
+	return length;
+}
 
-        if(tempIndex != -1){
-            prev[i] = tempIndex;
-            length[i] = length[tempIndex] + 1;
-        }
-
-        if(length[i] > maxLength){
-            maxLength = length[i];
-            maxLengthIndex = i;
-        }
-    }
-
-    //cout<<maxLengthIndex<<spc<<maxLength<<endl;
-    int lis[maxLength];
-    int j = maxLengthIndex;
-
-    for(int i = maxLength-1; i>=0; --i){
-        lis[i] = input[j];
-        j = prev[j];
-    }
-
-    cout<<"LIS length: "<<maxLength<<endl;
-    cout<<"LIS: ";
-    for(int i=0; i<maxLength; ++i)
-        cout<<lis[i]<<spc;
-    cout<<endl;
-
-    return 0;
+int main()
+{
+	std::vector<int> v{ 2, 5, 3, 7, 11, 8, 10, 13, 6 };
+	std::cout << "Length of Longest Increasing Subsequence is "
+			<< LongestIncreasingSubsequenceLength(v) << '\n';
+	return 0;
 }
